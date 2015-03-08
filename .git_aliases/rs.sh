c470bcc15e1cf5ec4be3fbd9cd856a2ef97c53ed
nikuz-devel <micurino@gmail.com>
2015-03-08 23:33:27 +0700

    Added git reset soft alias #3

---
+++ .git_aliases/rs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $DIR/_printColor.sh
+source $DIR/_help.sh
+
+function main() {
+  git reset --soft HEAD^
+}
+
+if ! helpMode $@ `basename $0`
+then
+  main $@
+fi
